3edf80e11b19bd0cd54a6777d64336ca6054018f
Scott C. Reynolds <scott@scottcreynolds.com>
2017-04-17 00:29:44 -0400

    day 3 wip

---
+++ my_script.js
@@ -0,0 +1,12 @@
+// var http = require("http");
+
+// http.createServer(function(request, response) {
+//   console.log(request);
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }).listen(8888);
+
+let square = x => x*x
+// debugger;
+console.log(square(4))
---
+++ node_debug.js
@@ -0,0 +1,6 @@
+var a = Math.floor(Math.random())
+if (a>=0.5) {
+  console.log('head')
+} else {
+  console.log('tail')
+}
